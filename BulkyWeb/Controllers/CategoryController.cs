dbe39672ec4fcc21e3ca3ec6d2b6271ffd503aa4
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-12-31 16:37:06 -0500

    Display Index View Data

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
    public class CategoryController : Controller
    {
+      private readonly ApplicationDbContext _db;
+      
+      public CategoryController(ApplicationDbContext db)
+      {
+            _db = db;
+      }
+
       public IActionResult Index()
       {
-         return View();
+         // Retrieves all Category records from the Db
+         List<Category> objCategoryList = _db.Categories.ToList();
+         
+         // Pass List of all categories to the Index view
+         return View(objCategoryList);
       }
    }
 }

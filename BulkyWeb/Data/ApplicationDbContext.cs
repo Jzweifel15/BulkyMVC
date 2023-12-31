ae7a441d848fc14013d9c427519938c7b61c77d4
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-12-31 16:13:32 -0500

    Seed Category Data

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,6 +10,16 @@ namespace BulkyWeb.Data
 
         }
 
-         public DbSet<Category> Categories { get; set; }
-   }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+        }
+    }
 }

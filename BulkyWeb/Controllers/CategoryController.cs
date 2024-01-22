4883933f4c33e26be326613db55c1f9dc9cbd168
Justin R. Zweifel <jzweifel15@yahoo.com>
2024-01-21 19:36:39 -0500

    Add 'Edit' actions in Category Controller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -46,5 +46,36 @@ namespace BulkyWeb.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+
+            /** Other ways to find the model
+             *  
+             *  Category? category = _db.Categories.FirstOrDefault(cat => cat.Id == id);
+             *  Category? category = _db.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+             *  
+             */
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit()
+        {
+            return View();
+        }
     }
 }
